1eb348973b749c2508dd0536e18db5f66c2b4929
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2020-07-05 19:18:45 +0300

    Introduce medline plugin

--- lib/rayyan-formats-plugins.rb
+++ lib/rayyan-formats-plugins.rb
@@ -7,6 +7,7 @@ require "rayyan-formats-plugins/plugins/refman"
 require "rayyan-formats-plugins/plugins/word_doc"
 require "rayyan-formats-plugins/plugins/zip"
 require "rayyan-formats-plugins/plugins/ciw"
+require "rayyan-formats-plugins/plugins/medline"
 
 module RayyanFormats
   module Plugins
