f757b7340896b32a50c74a49aa178e45b2355478
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2020-07-07 13:01:49 +0300

    Introduce plugin for medline (.nbib) format (#22)
    * Introduce medline plugin
    * Add spec support files for medline
    * Add test cases for medline
    * Bump version to 0.2.0
    * Emit PMID even if target.sid is nil

---
+++ lib/rayyan-formats-plugins/plugins/medline.rb
@@ -0,0 +1,189 @@
+require 'ref_parsers'
+
+module RayyanFormats
+  module Plugins
+    class MEDLINE < RayyanFormats::Base
+      title 'MEDLINE'
+      extension 'nbib'
+      description 'Supports the Pubmed MEDLINE format as outlined here: https://www.nlm.nih.gov/bsd/mms/medlineelements.html'
+
+      detect do |first_line, _|
+        first_line.start_with?('PMID')
+      end
+
+      do_import do |body, _, &block|
+        articles = ::RefParsers::PubMedParser.new.parse(body)
+        total = articles.length
+
+        articles.each do |article|
+          target = Target.new
+          # Convert PT to Array in case only one PT
+          target.publication_types = Array(article['PT'])
+          # Accession number is the first line and hence is in article['type]
+          target.sid = article['type']
+          target.title = article['TI']&.gsub(/\s+/, ' ')
+          target.date_array = get_date_array article['DP']
+          target.journal_title = article['JT']
+          target.journal_issn = get_issn article['IS']
+          target.journal_abbreviation = article['TA']
+          # to_i grabs the first integer it sees, so '5 Spec No' becomes 5
+          target.jvolume = article['VI']&.to_i
+          target.jissue = article['IP']&.to_i
+          target.pagination = article['PG']
+          target.authors = Array(article['AU'])
+          target.affiliation = get_multivalued_field_with_newline_merger article['AD']
+          # Generates url from pmid stored in article['type']
+          target.url = get_pubmed_url article['type']
+          target.language = article['LA']
+          target.publisher_location = article['PL']
+          target.collection = article['CTI']
+          target.keywords = Array(article['MH'])
+          target.abstracts = get_multivalued_field_with_newline_merger article['AB']
+          target.notes = article['GN']
+          target.article_ids = get_article_ids article
+
+          block.call(target, total)
+        end
+      end
+
+      do_export do |target, options|
+        [
+          "PMID- #{target.sid}\n",
+          emit_lines_from_array('PT', target.publication_types),
+          emit_line('TI', target.title),
+          emit_line('DP', format_date_array(target.date_array)),
+          emit_line('JT', target.journal_title),
+          emit_line('IS', target.journal_issn),
+          emit_line('TA', target.journal_abbreviation),
+          emit_line('VI', target.jvolume),
+          emit_line('IP', target.jissue),
+          emit_line('PG', target.pagination),
+          emit_lines_from_array('AU', target.authors),
+          emit_lines_from_array('AD', target.affiliation),
+          emit_lines_from_array('LA', target.language),
+          emit_line('PL', target.publisher_location),
+          emit_line('CTI', target.collection),
+          emit_lines_from_array('MH', target.keywords),
+          get_abstracts(target, options) { |abstracts| emit_lines_from_array('AB', abstracts) },
+          emit_lines_from_array('GN', target.notes),
+          emit_article_id('PMC', :pmc_id, target.article_ids),
+          emit_article_id('AID', :doi, target.article_ids),
+          emit_article_id('AID', :pii, target.article_ids),
+          "\n"
+        ].flatten.join if target
+      end
+
+      class << self
+        def get_date_array(date_line)
+          # Converts MEDLINE date format to date array
+          # Example dates in the MEDLINE format:
+          # 2001 Apr 15
+          # 2001 Apr
+          # 2000 Spring
+          # 2000 Nov-Dec
+          # 2001
+          return unless date_line
+
+          splitted_date_line = date_line.split(' ')
+
+          year = splitted_date_line[0]
+          month = (convert_abbr_month_name_to_number splitted_date_line[1])&.to_s
+          day = splitted_date_line[2]
+
+          return [] if year.nil?
+          return [year] if month.nil?
+          return [year, month] if day.nil?
+
+          [year, month, day]
+        end
+
+        def convert_abbr_month_name_to_number(month_name)
+          # Converts abbreviated month names to month number
+          return unless month_name
+
+          month_name_to_convert = month_name
+          # Check if month name is something like Nov-Dec
+          month_name_to_convert = month_name_to_convert.split('-')[0] if month_name_to_convert.include? '-'
+          month_number = Date::ABBR_MONTHNAMES.index(month_name_to_convert)
+
+          month_number unless month_number.nil?
+        end
+
+        def get_pubmed_url(pmid)
+          return unless pmid
+
+          "https://pubmed.ncbi.nlm.nih.gov/#{pmid}/"
+        end
+
+        def get_article_ids(article)
+          article_ids = []
+          article_ids.push( idtype: :pubmed_id, value: article['type'] ) if article['type']
+          article_ids.push(idtype: :pmc_id, value: article['PMC']) if article['PMC']
+          return article_ids unless article['AID']
+
+          # A single AID line is parsed as a string, turn it into an array
+          # Multiple AID lines are also possible
+          aid_lines = [article['AID']].flatten
+
+          aid_lines.each do |line|
+            article_ids.push(idtype: :doi, value: line) if line.include? 'doi'
+            article_ids.push(idtype: :pii, value: line) if line.include? 'pii'
+          end
+
+          article_ids
+        end
+
+        def get_issn(issn)
+          # MEDLINE returns multiple ISSN, return the first one
+          return unless issn
+
+          issn.is_a?(Array) ? issn[0] : issn
+        end
+
+        def get_multivalued_field_with_newline_merger(values)
+          return unless values
+
+          # If single-valued string field, convert to array
+          values_to_get = Array(values)
+
+          values_to_get.map! do |value|
+            value.gsub(/\s+/, ' ')
+          end
+        end
+
+        def format_date_array(date_array)
+          # Converts date array to MEDLINE date format
+          # Eg date_array = ["2017", "10", "1"]
+          year = date_array[0]
+          month = Date::ABBR_MONTHNAMES[date_array[1].to_i] if date_array[1]
+          day = date_array[2]
+
+          return '' if year.nil?
+          return "#{year}" if month.nil?
+          return "#{year} #{month}" if day.nil?
+
+          "#{year} #{month} #{day}"
+        end
+
+        def emit_line(key, value)
+          # Pad key to 4 characters to match nbib
+          padded_key = format('%-4.4s', key)
+          "#{padded_key}- #{value}\n" unless value.nil? || value.to_s.strip.empty?
+        end
+
+        def emit_lines_from_array(key, values_arr)
+          return unless values_arr
+
+          Array(values_arr).map { |val| emit_line(key, val) }
+        end
+
+        def emit_article_id(key, article_id_type, article_ids)
+          return unless article_ids
+
+          aid_obj_to_emit = article_ids.detect { |aid_obj| aid_obj[:idtype] == article_id_type }
+          emit_line(key, aid_obj_to_emit[:value]) if aid_obj_to_emit
+        end
+      end
+    end
+  end
+end
---
+++ spec/plugins/medline_spec.rb
@@ -0,0 +1,288 @@
+require 'spec_helper'
+
+include RayyanFormats::Plugins
+
+describe MEDLINE do
+  describe '.detect' do
+    it 'returns false if file does not start with PMID line' do
+      expect(MEDLINE.send(:detect, '1234', [])).to eq(false)
+    end
+
+    it 'returns true if file starts with PMID line' do
+      expect(MEDLINE.send(:detect, 'PMID- 287654', [])).to eq(true)
+    end
+  end
+
+  describe '.do_import' do
+    let(:filename) { 'spec/support/example1.nbib' }
+    let(:body) { File.read(filename, mode: 'r:bom|UTF-8') }
+    let(:expected_total) { 10 }
+    let(:plugin) { MEDLINE }
+
+    it_behaves_like 'repetitive target yielder'
+
+    it 'assigns correct values' do
+      line = 0
+      MEDLINE.send(:do_import, body, filename) do |target|
+        case line
+        when 0
+          expect(target.publication_types).to eq(['Journal Article', 'Review'])
+          expect(target.sid).to eq('29952495')
+          expect(target.title).to eq('Breast cancer: basics, screening, diagnostics and treatment.')
+          expect(target.date_array).to eq(%w[2017 2])
+          expect(target.journal_title).to eq('Medizinische Monatsschrift fur Pharmazeuten')
+          expect(target.journal_issn).to eq('0342-9601 (Print)')
+          expect(target.journal_abbreviation).to eq('Med Monatsschr Pharm')
+          expect(target.jvolume).to eq(40)
+          expect(target.jissue).to eq(2)
+          expect(target.pagination).to eq('55-64')
+          expect(target.authors).to eq(['Wörmann B'])
+          expect(target.affiliation).to eq(['Medizinische Klinik mit Schwerpunkt Hämatologie'])
+          expect(target.url).to eq('https://pubmed.ncbi.nlm.nih.gov/29952495/')
+          expect(target.language).to eq(%w[eng ger])
+          expect(target.publisher_location).to eq('Germany')
+          expect(target.collection).to eq('Breast Cancer Reviews')
+          expect(target.keywords).to eq(['Combined Modality Therapy', 'Early Detection of Cancer', 'Female', 'Humans', 'Mass Screening'])
+          expect(target.abstracts[0]).to start_with('Breast cancer is by far the most common malignancy in women. The median age is 64 years')
+          expect(target.notes).to eq(['KIE: Article and commentaries', 'KIE: KIE BoB Subject Heading: health care'])
+          expect(target.article_ids).to eq([idtype: :pubmed_id, value: '29952495'])
+        when 1
+          expect(target.publication_types).to eq(['Journal Article', 'Review'])
+          expect(target.sid).to eq('28298516')
+          expect(target.title).to eq('Precision Medicine in Breast Cancer.')
+          expect(target.date_array).to eq(%w[2017 3])
+          expect(target.journal_title).to eq('Radiologic technology')
+          expect(target.journal_issn).to eq('1943-5657 (Electronic)')
+          expect(target.journal_abbreviation).to eq('Radiol Technol')
+          expect(target.jvolume).to eq(88)
+          expect(target.jissue).to eq(4)
+          expect(target.pagination).to eq('401M-421M')
+          expect(target.authors).to eq(['Odle TG'])
+          expect(target.affiliation).to eq(nil)
+          expect(target.url).to eq('https://pubmed.ncbi.nlm.nih.gov/28298516/')
+          expect(target.language).to eq('eng')
+          expect(target.publisher_location).to eq('United States')
+          expect(target.collection).to eq('Breast Cancer Reviews')
+          expect(target.keywords).to eq(
+            ['Biomarkers, Tumor', 'Breast Neoplasms/*diagnosis/*genetics/*therapy', 'Female', '*Genomics', 'Humans', '*Precision Medicine']
+          )
+          expect(target.abstracts[0]).to start_with(
+            'Breast cancer care has improved markedly in recent decades, but new advancements in ' +
+            'diagnosis and treatment depend on translating genomics and precision medicine into ' +
+            'clinical care'
+          )
+          expect(target.notes).to eq('KIE: Article and commentaries')
+          expect(target.article_ids).to eq(
+            [
+              { idtype: :pubmed_id, value: '28298516' },
+              { idtype: :pii, value: '88/4/401M [pii]' }
+            ]
+          )
+        when 2
+          expect(target.publication_types).to eq(['Introductory Journal Article'])
+          expect(target.sid).to eq('20521754')
+          expect(target.title).to eq('Treatment of breast cancer.')
+          expect(target.date_array).to eq(%w[2010 6 1])
+          expect(target.journal_title).to eq('American family physician')
+          expect(target.journal_issn).to eq('1532-0650 (Electronic)')
+          expect(target.journal_abbreviation).to eq('Am Fam Physician')
+          expect(target.jvolume).to eq(81)
+          expect(target.jissue).to eq(11)
+          expect(target.pagination).to eq('1339-46')
+          expect(target.authors).to eq(['Maughan KL', 'Lutterbie MA', 'Ham PS'])
+          expect(target.affiliation).to eq(
+            ['Department of Family Medicine, University of Virginia School of Medicine, ' +
+            'Charlottesville, VA 22908, USA. kmaughan@virginia.edu']
+          )
+          expect(target.url).to eq('https://pubmed.ncbi.nlm.nih.gov/20521754/')
+          expect(target.language).to eq('eng')
+          expect(target.publisher_location).to eq('United States')
+          expect(target.collection).to eq(nil)
+          expect(target.keywords).to eq(
+            ['Antineoplastic Agents/therapeutic use', 'Breast Neoplasms/drug therapy/pathology/radiotherapy/surgery/*therapy',
+              'Combined Modality Therapy', 'Female', 'Humans', 'Mastectomy', 'Neoplasm Staging', 'Sentinel Lymph Node Biopsy']
+          )
+          expect(target.abstracts[0]).to start_with(
+            'Understanding breast cancer treatment options can help family physicians care for ' +
+            'their patients during and after cancer treatment. This article reviews typical'
+          )
+          expect(target.notes).to eq(nil)
+          expect(target.article_ids).to eq(
+            [
+              { idtype: :pubmed_id, value: '20521754' },
+              { idtype: :pii, value: 'd8230 [pii]' }
+            ]
+          )
+        when 3
+          expect(target.publication_types).to eq(['Journal Article'])
+          expect(target.sid).to eq('27533387')
+          expect(target.title).to eq('Title that spans multiple lines for this particular research article')
+          expect(target.date_array).to eq(%w[2000 11])
+          expect(target.journal_issn).to eq('2047-9018 (Electronic)')
+          expect(target.authors).to eq(['Pearce L'])
+          expect(target.article_ids).to eq(
+            [
+              { idtype: :pubmed_id, value: '27533387' },
+              { idtype: :doi, value: '10.7748/ns.30.51.15.s16 [doi]' },
+            ]
+          )
+        when 4
+          expect(target.publication_types).to eq(['Journal Article', 'Review'])
+          expect(target.sid).to eq('29284222')
+          expect(target.date_array).to eq(%w[2017 12 23])
+          expect(target.authors).to eq(
+            ['Kolak A', 'Kamińska M', 'Sygit K', 'Budny A', 'Surdyka D', 'Kukiełka-Budny B', 'Burdan F']
+          )
+          expect(target.affiliation).to eq(
+            [
+              "St. John's Cancer Center, Department of Radiotherapy, Lublin, Poland. agkola@interia.pl.",
+              "St. John's Cancer Center, Department of Oncology, Lublin, Poland.",
+              'University of Szczecin, Faculty of Physical Education and Health Promotion, Szczecin, Poland.',
+              "St. John's Cancer Center, Department of Radiotherapy, Lublin, Poland.",
+              "St. John's Cancer Center, Department of Radiotherapy, Lublin, Poland.",
+              "St. John's Cancer Center, Department of Oncology, Lublin, Poland.",
+              'Human Anatomy Department, Medical Univeristy of Lublin, Poland.'
+            ]
+          )
+          expect(target.article_ids).to eq(
+            [
+              { idtype: :pubmed_id, value: '29284222' },
+              { idtype: :pii, value: '75943 [pii]' },
+              { idtype: :doi, value: '10.26444/aaem/75943 [doi]' }
+            ]
+          )
+        when 5
+          expect(target.publication_types).to eq(['Journal Article', 'Review'])
+          expect(target.sid).to eq('21969133')
+          expect(target.date_array).to eq(%w[2011 11])
+          expect(target.authors).to eq(['DeSantis C', 'Siegel R', 'Bandi P', 'Jemal A'])
+          expect(target.affiliation).to eq(
+            ['Epidemiologist, Surveillance Research, American Cancer Society, Atlanta, GA 30303, USA. carol.desantis@cancer.org']
+          )
+          expect(target.article_ids).to eq(
+            [
+              { idtype: :pubmed_id, value: '21969133' },
+              { idtype: :doi, value: '10.3322/caac.20134 [doi]' }
+            ]
+          )
+        end
+        line += 1
+      end
+    end
+  end
+
+  describe '.do_export' do
+    let(:plugin) { MEDLINE }
+    let(:target) {
+      t = RayyanFormats::Target.new
+      t.publication_types = ['Journal Article', 'Review']
+      t.sid = '123456'
+      t.title = 'A title for a research article'
+      t.date_array = %w[2020 5 20]
+      t.journal_title = 'A journal title'
+      t.journal_issn = 'A journal issn'
+      t.journal_abbreviation = 'A journal abbreviation'
+      t.jvolume = 1
+      t.jissue = 10
+      t.pagination = 'pagination1'
+      t.authors = ['a1_last_name a1_first_name', 'a2_last_name a2_first_name']
+      t.affiliation = %w[University1 University2]
+      t.language = %w[eng ger]
+      t.publisher_location = 'publisher location'
+      t.collection = 'collection1'
+      t.keywords = %w[kw1 kw2 kw3 kw4]
+      t.abstracts = ['This is abstract 1', 'This is abstract 2']
+      t.notes = ['This is note1', 'This is note2']
+      t
+    }
+
+    let(:target_s) {
+      File.read('spec/support/example2.nbib')
+    }
+
+    let(:target_s_abstracts) {
+      File.read('spec/support/example3.nbib')
+    }
+
+    it_behaves_like "correct target emitter"
+  end
+
+  describe '.get_date_array' do
+    it 'returns correct dates' do
+      expect(MEDLINE.get_date_array('2010')).to eq(%w[2010])
+      expect(MEDLINE.get_date_array('2007 Jan')).to eq(%w[2007 1])
+      expect(MEDLINE.get_date_array('2011 Jun 2')).to eq(%w[2011 6 2])
+      expect(MEDLINE.get_date_array('2016 Oct-Dec')).to eq(%w[2016 10])
+      expect(MEDLINE.get_date_array('2020 random 3')).to eq(%w[2020])
+    end
+  end
+
+  describe '.convert_abbr_month_name_to_number' do
+    it 'returns correct month number' do
+      expect(MEDLINE.convert_abbr_month_name_to_number('Feb')).to eq(2)
+      expect(MEDLINE.convert_abbr_month_name_to_number('May')).to eq(5)
+      expect(MEDLINE.convert_abbr_month_name_to_number('Jun')).to eq(6)
+      expect(MEDLINE.convert_abbr_month_name_to_number('December')).to eq(nil)
+      expect(MEDLINE.convert_abbr_month_name_to_number('random')).to eq(nil)
+    end
+  end
+
+  describe '.get_article_ids' do
+    it 'returns correct article ids' do
+      article = {
+        'type' => '123456',
+        'PMC' => 'pmcid',
+        'AID' => ['some doi number', 'some pii number']
+      }
+
+      expect(MEDLINE.get_article_ids(article)).to eq(
+        [
+          { idtype: :pubmed_id, value: '123456' },
+          { idtype: :pmc_id, value: 'pmcid' },
+          { idtype: :doi, value: 'some doi number' },
+          { idtype: :pii, value: 'some pii number' }
+        ]
+      )
+    end
+  end
+
+  describe '.format_date_array' do
+    it 'formats date array correctly' do
+      expect(MEDLINE.format_date_array(['2020'])).to eq('2020')
+      expect(MEDLINE.format_date_array(%w[2006 3])).to eq('2006 Mar')
+      expect(MEDLINE.format_date_array(%w[2012 12 15])).to eq('2012 Dec 15')
+    end
+  end
+
+  describe '.emit_line' do
+    it 'emits line with key correctly' do
+      expect(MEDLINE.emit_line('TI', 'A title')).to eq("TI  - A title\n")
+      expect(MEDLINE.emit_line('PMID', '123456')).to eq("PMID- 123456\n")
+    end
+  end
+
+  describe '.emit_lines_from_array' do
+    it 'emits lines from array elements correctly' do
+      expect(MEDLINE.emit_lines_from_array('AU', %w[author1 author2 author3])).to eq(
+        ["AU  - author1\n", "AU  - author2\n", "AU  - author3\n"]
+      )
+      expect(MEDLINE.emit_lines_from_array('AB', 'abstract1')).to eq(
+        ["AB  - abstract1\n"]
+      )
+    end
+  end
+
+  describe '.emit_article_id' do
+    it 'emits article ids' do
+      article_ids = [
+        { idtype: :pubmed_id, value: '123456' },
+        { idtype: :pmc_id, value: 'pmcid' },
+        { idtype: :doi, value: 'some doi number' },
+        { idtype: :pii, value: 'some pii number' }
+      ]
+      expect(MEDLINE.emit_article_id('PMC', :pmc_id, article_ids)).to eq("PMC - pmcid\n")
+      expect(MEDLINE.emit_article_id('AID', :doi, article_ids)).to eq("AID - some doi number\n")
+      expect(MEDLINE.emit_article_id('AID', :pii, article_ids)).to eq("AID - some pii number\n")
+    end
+  end
+end
