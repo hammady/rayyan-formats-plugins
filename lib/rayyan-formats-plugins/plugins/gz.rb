c7b61a0df47fc85a6aae20d551ee7f061ff55b8c
Hossam Hammady <github@hammady.net>
2020-11-24 13:26:25 +0300

    Implement import conversion logic for GZ and Zip plugins

--- lib/rayyan-formats-plugins/plugins/gz.rb
+++ lib/rayyan-formats-plugins/plugins/gz.rb
@@ -3,12 +3,12 @@ require 'zlib'
 module RayyanFormats
   module Plugins
     class GZ < RayyanFormats::Base
-      
+
       title 'GZ'
       extension 'gz'
       description 'Supports a single compressed file in any of the core formats. Example: file.csv.gz, file.ris.gz'
 
-      do_import do |body, filename, &block|
+      do_import do |body, filename, converter, &block|
         filename.gsub!(/\.gz$/, '')
         ext = File.extname(filename).delete('.')
         ext = 'txt' if ext.length == 0
@@ -17,9 +17,10 @@ module RayyanFormats
         gzipped = Zlib::GzipReader.new(fileContent)
         fileContent = StringIO.new(gzipped.read).string
         gzipped.close
-        plugin.do_import(fileContent, filename, &block)
+        fileContent = converter.call(fileContent, ext) if converter
+        plugin.do_import(fileContent, filename, converter, &block)
       end
-      
+
     end # class
   end # module
 end # module
